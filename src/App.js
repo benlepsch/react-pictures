a46f0a8c3465b99612c9b6433d00344e24ed662f
Ben Lepsch <blepsch@gmail.com>
2025-05-02 10:11:25 -0400

    try this maybe

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import { React, useState } from 'react';
 import { NavLink, Routes, Route } from 'react-router-dom';
 
-export default function App () {
+function App() {
     return (
-    <>
-        <p>will this work? who knows</p>
-        <Navigation />
-        <Main />
-    </>
+        <>
+            <p>will this work? who knows</p>
+            <Navigation />
+            <Main />
+        </>
     );
 }
 
-const Navigation = () => (
-    <>
-        <NavLink to='/'> home</NavLink>
-        <NavLink to='/fart'>log in</NavLink>
-    </>
-);
+function Navigation() {
+    return (
+        <ul>
+            <li><NavLink to='/'> home</NavLink></li>
+            <li><NavLink to='/fart'>log in</NavLink></li>
+        </ul>
+    );
+}
 
-const Main = () => (
-    <Routes>
-        <Route exact path='/' component={Home}></Route>
-        <Route exact path='/fart' component={Fart}></Route>
-    </Routes>
-);
+function Main() {
+    return (
+        <Routes>
+            <Route exact path='/' component={Home}></Route>
+            <Route exact path='/fart' component={Fart}></Route>
+        </Routes>
+    );
+}
 
-const Home = () => (
-    <p>what is this</p>
-);
+function Home() {
+    return (
+        <p>what is this</p>
+    );
+}
 
-const Fart = () => (
-    <p>ahahaha you thought you could log in? its a static server dummy</p>
-);
+function Fart() {
+    return (
+        <p>ahahaha you thought you could log in? its a static server dummy</p>
+    );
+}
+
+
+export default App;

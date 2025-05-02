d1b21b243ea9da6ff1eecf57fac898a41fb2243d
Ben Lepsch <blepsch@gmail.com>
2025-05-02 14:47:17 +0000

    works with tweaks

--- src/pages/Frame.js
+++ src/pages/Frame.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 const PICTURE_DATA = [
     { 
-        src: "%PUBLIC_URL%/images/IMG_7079.jpg",
+        src: "images/IMG_7079.jpg",
         alt: "Gorham mountain, the Beehive, sand beach, & Great Head. Acadia NP, ME",
     },
 ];
@@ -26,28 +26,28 @@ function Frame() {
     const [pn, setPn] = useState(0);
 
     function inc() {
-        const next = (pn < PICUTRE_DATA.length - 1) ? pn + 1 : 0;
+        const next = (pn < PICTURE_DATA.length - 1) ? pn + 1 : 0;
         setPn(next);
         // console.log('adding to pn, now ' + pn);
     }
 
     function dec() {
-        const next = (pn > 0) ? pn - 1 : PICUTRE_DATA.length - 1;
+        const next = (pn > 0) ? pn - 1 : PICTURE_DATA.length - 1;
         setPn(next);
         // console.log('subtracting from pn, now ' + pn);
     }
 
     function rand() {
-        const next = Math.floor(Math.random() * PICUTRE_DATA.length);
+        const next = Math.floor(Math.random() * PICTURE_DATA.length);
         setPn(next);
     }
 
     return (
         <div class="flexy flex-col container">
             <div class="flexy imgc">
-                <img alt={PICUTRE_DATA[pn].alt} src={PICUTRE_DATA[pn].src} />
+                <img alt={PICTURE_DATA[pn].alt} src={PICTURE_DATA[pn].src} />
             </div>
-            <Caption text={PICUTRE_DATA[pn].alt} />
+            <Caption text={PICTURE_DATA[pn].alt} />
             <div class="flexy flex-row wide">
             <Arrow text={"Back"} handleClick={dec} />
                 <Arrow text={"Random"} handleClick={rand} />

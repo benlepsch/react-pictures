193c852bc74beecc1b0db1ece011af907c3e0d17
Ben Lepsch <blepsch@gmail.com>
2025-05-02 09:56:59 -0400

    try this

--- src/App.js
+++ src/App.js
@@ -1,81 +1,32 @@
 import { useState } from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
+import { NavLink, Switch, Route } from 'react-router-dom';
 
-function Arrow({ text, handleClick }) {
-    return (
-        <button
-            onClick={handleClick}
-            class="flexy"
-        >
-           {text}
-        </button>
-    );
-}
-
-function Caption({ text }) {
-    return (<p class="flexy">{text}</p>);
-}
-
-export default function Frame() {
-    const [pn, setPn] = useState(0);
-    
-    let pics = [];
-    let alts = [];
-    const picHolder = document.querySelectorAll('.image');
-    // console.log(picHolder);
-
-    for (let i = 0; i < picHolder.length; i++) {
-        // console.log('adding picture: ' + picHolder[i].src);
-        pics.push(picHolder[i].src);
-        alts.push(picHolder[i].alt);
-    }
-    
-    function inc() {
-        const next = (pn < picHolder.length - 1) ? pn + 1 : 0;
-        setPn(next);
-        // console.log('adding to pn, now ' + pn);
-    }
-
-    function dec() {
-        const next = (pn > 0) ? pn - 1 : pics.length - 1;
-        setPn(next);
-        // console.log('subtracting from pn, now ' + pn);
-    }
-
-    function rand() {
-        const next = Math.floor(Math.random() * pics.length);
-        setPn(next);
-    }
-
-
-    return (
+const App = () => (
     <>
-        <div class="flexy flex-col container">
-            <div class="flexy imgc">
-                <img alt={alts[pn]} src={pics[pn]} />    
-            </div>
-            <Caption text={alts[pn]} /> 
-            <div class="flexy flex-row wide">
-                <Arrow text={"Back"} handleClick={dec} />
-                <Arrow text={"Random"} handleClick={rand} />
-                <Arrow text={"Next"} handleClick={inc} />
-            </div>
-        </div>
-        <Link to="/fart">To fart</Link>
+        <p>will this work? who knows</p>
+        <Navigation />
         <Main />
     </>
-    );
-}
+);
 
-const Fart = () => {
-    return <Link to="/">To main</Link>
-}
+const Navigation = () => (
+    <>
+        <NavLink to='/'> home</NavLink>
+        <NavLink to='/fart'>log in</NavLink>
+    </>
+);
 
-const Main = () => {
-    return (
-        <Routes>
-            <Route exact path="/" element={<Frame />} />
-            <Route exact path="/fart" element={<Fart />} />
-        </Routes>
-    );
-}
+const Main = () => (
+    <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/fart' component={Fart}></Route>
+    </Switch>
+);
+
+const Home = () => (
+    <p>what is this</p>
+);
+
+const Fart = () => (
+    <p>ahahaha you thought you could log in? its a static server dummy</p>
+);

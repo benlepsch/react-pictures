d6d72ece13fffe299109f750410fa561be23d404
Ben Lepsch <blepsch@gmail.com>
2025-05-08 14:25:33 +0000

    it's so broken

--- src/pages/Minesweeper.js
+++ src/pages/Minesweeper.js
@@ -1,8 +1,49 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 
-// function Board() {
+import './minesweeper.css';
 
-// }
+const Vis = {
+    Cleared: 'clear',
+    Flagged: 'flag',
+    Hidden: 'hide',
+}
+
+function Cell({ x, y, bombs, vis }) {
+    const style = 'cell ' + vis[x][y];
+
+    return <div class={style}></div>;
+}
+
+function Row({ j, width, bombs, vis }) {
+    const cells = [];
+
+    for (let i = 0; i < width; i++) {
+        cells.push(<Cell x={i} y={j} bombs={bombs} vis={vis} />);
+    }
+
+    return <>{cells}</>;
+}
+
+function Board({ width, height }) {
+    const vis = [[]];
+    const bombs = [[]];
+    const board = [];
+
+    for (let j = 0; j < height; j++) {
+        const visRow = [];
+        const bombsRow = [];
+
+        for (let i = 0; i < width; i++) {
+            visRow.push(Vis.Hidden);
+        }
+    }
+
+    for (let j = 0; j < height; j++) {
+        board.push(<Row width={width} j={j} bombs={bombs} vis={vis} />);
+    }
+
+    return <>{board}</>;
+}
 
 function Minesweeper() {
     document.title = 'Minesweeper';
@@ -18,13 +59,13 @@ function Minesweeper() {
 
     if (running) {
         return (<>
-            <p>Game running</p>
-            <button onclick={endGame}>Quit</button>
+            <Board width={10} height={10} />
+            <button onClick={endGame}>Quit</button>
         </>);
     } else {
         return (<>
             <p>Game not running</p>
-            <button onclick={startGame}>Start</button>
+            <button onClick={startGame}>Start</button>
         </>);
     }
 }

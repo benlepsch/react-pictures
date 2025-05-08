b3302ef7ffef37a80358cf9dc40ebbaaa7c78a10
Ben Lepsch <blepsch@gmail.com>
2025-05-08 14:38:37 -0400

    i hate this

--- src/pages/Minesweeper.js
+++ src/pages/Minesweeper.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { useState } from 'react';
 
 import './minesweeper.css';
 
@@ -8,29 +8,8 @@ const Vis = {
     Hidden: 'cell hide',
 }
 
-function Cell({ x, y, bombs, handleClick }) {
-    let number = 'B';
-    let num_temp = 0;
-    if (!bombs[x][y]) {
-        const lx = (x == 0) ? x : x - 1;
-        const hx = (x == bombs.length-1) ? x + 1 : x + 2;
-        const ly = (y == 0) ? y : y - 1;
-        const hy = (y == bombs[0].length-1) ? y + 1 : y + 2;
-
-        // console.log('i am square (' + x + ',' + y + ') checking ' + lx + ' to ' + hx + ' and ' + ly + ' to ' + hy);
-
-        for (let i = lx; i < hx; i++) {
-            for (let j = ly; j < hy; j++) {
-                num_temp += bombs[i][j];
-            }
-        }
-        
-        if (num_temp !== 0) {
-            number = num_temp.toString();
-        } else {
-            number = '';
-        }
-    }
+function Cell({ x, y, bomb, close, handleClick }) {
+    let number = (bomb) ? 'B' : (close == 0) ? '' : close.toString();
     
     const clicked = (e) => {
         e.preventDefault();
@@ -50,16 +29,38 @@ function Cell({ x, y, bombs, handleClick }) {
     );
 }
 
-function Col({ x, height, bombs, handleClick }) {
+function Col({ x, height, bombs, nums, handleClick }) {
     const cells = [];
 
     for (let j = 0; j < height; j++) {
-        cells.push(<Cell x={x} y={j} bombs={bombs} handleClick={handleClick} />);
+        cells.push(<Cell x={x} y={j} bomb={bombs[j]} close={nums[j]} handleClick={handleClick} />);
     }
 
     return <div class="column">{cells}</div>;
 }
 
+    // let number = 'B';
+    // let num_temp = 0;
+    // if (!bombs[x][y]) {
+    //     const lx = (x == 0) ? x : x - 1;
+    //     const hx = (x == bombs.length-1) ? x + 1 : x + 2;
+    //     const ly = (y == 0) ? y : y - 1;
+    //     const hy = (y == bombs[0].length-1) ? y + 1 : y + 2;
+
+    //     // console.log('i am square (' + x + ',' + y + ') checking ' + lx + ' to ' + hx + ' and ' + ly + ' to ' + hy);
+
+    //     for (let i = lx; i < hx; i++) {
+    //         for (let j = ly; j < hy; j++) {
+    //             num_temp += bombs[i][j];
+    //         }
+    //     }
+        
+    //     if (num_temp !== 0) {
+    //         number = num_temp.toString();
+    //     } else {
+    //         number = '';
+    //     }
+    // }
 function Board({ width, height }) {
     function handleClick(event, x, y) {
         if (event.type === 'click') {
@@ -79,6 +80,7 @@ function Board({ width, height }) {
 
     const vis = [];
     const bombs = [];
+    const numbers = [];
     const board = [];
 
     for (let i = 0; i < width; i++) {
@@ -92,16 +94,36 @@ function Board({ width, height }) {
 
         vis.push(visCol);
         bombs.push(bombsCol);
+        numbers.push(new Array(height));
     }
 
-    bombs[0][0] = true;
-    bombs[2][4] = true;
-    bombs[4][4] = true;
-    bombs[0][7] = true;
-    bombs[0][8] = true;
+    function addBomb(x, y) {
+        bombs[x][y] = true;
+
+        // update adjacent squares
+        const lx = (x == 0) ? x : x - 1;
+        const hx = (x == bombs.length-1) ? x + 1 : x + 2;
+        const ly = (y == 0) ? y : y - 1;
+        const hy = (y == bombs[0].length-1) ? y + 1 : y + 2;
+
+        for (let i = 0; i < lx; i++) {
+            for (let j = 0; j < ly; j++) {
+                if ((i !== x) && (j !== y)) {
+                    numbers[i][j] += 1;
+                }
+            }
+        }
+    }
+
+    addBomb(0, 0);
+    addBomb(2, 4);
+    addBomb(4, 4);
+    addBomb(0, 7);
+    addBomb(0, 8);
+    addBomb(6, 0);
 
     for (let i = 0; i < width; i++) {
-        board.push(<Col x={i} height={height} bombs={bombs} handleClick={handleClick} />);
+        board.push(<Col x={i} height={height} bombs={bombs[i]} nums={numbers[i]} handleClick={handleClick} />);
     }
 
     return <div class="board">{board}</div>;
